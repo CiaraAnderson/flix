8cef21aff4cc9d8dcdc1a7f086ccce2f0d367354
Ciara Anderson <thelaylinguist@gmail.com>
2019-08-14 03:13:06 +0000

    Permissions

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,14 +1,24 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_enrolled_in_current_course, only: [:show]
+
+
   def show
+    
   end
 
 
 private
+
+def require_enrolled_in_current_course
+  if current_user.enrolled_in? != current_lesson.section.course
+    redirect_to course_path(current_course), alert: 'Error Message Here'
+  end
 
 helper_method :current_lesson
 def current_lesson
   @current_lesson ||= Lesson.find(params[:id])
 end
 
-
 end
+
